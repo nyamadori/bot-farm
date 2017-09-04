4a9cf3cc99dcc23b858fa292ee25a9ea5f52d744
nyamadori <nyamadorig@gmail.com>
2017-09-04 23:36:00 +0900

    Add bot model

---
+++ db/migrate/20170904143418_create_bots.rb
@@ -0,0 +1,10 @@
+class CreateBots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bots do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,66 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170904143418) do
+
+  create_table "bots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "channel_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "channel_id", null: false
+    t.bigint "member_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id", "member_id"], name: "index_channel_members_on_channel_id_and_member_id", unique: true
+    t.index ["channel_id"], name: "index_channel_members_on_channel_id"
+    t.index ["member_id"], name: "index_channel_members_on_member_id"
+  end
+
+  create_table "channels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "slack_id", null: false
+    t.bigint "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id", "slack_id"], name: "index_channels_on_team_id_and_slack_id", unique: true
+    t.index ["team_id"], name: "index_channels_on_team_id"
+  end
+
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "slack_id", null: false
+    t.bigint "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id", "slack_id"], name: "index_members_on_team_id_and_slack_id", unique: true
+    t.index ["team_id"], name: "index_members_on_team_id"
+  end
+
+  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "slack_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_teams_on_name"
+    t.index ["slack_id"], name: "index_teams_on_slack_id", unique: true
+  end
+
+  add_foreign_key "channel_members", "channels"
+  add_foreign_key "channel_members", "members"
+  add_foreign_key "channels", "teams"
+  add_foreign_key "members", "teams"
+end
