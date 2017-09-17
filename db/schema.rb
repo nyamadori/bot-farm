7d9e967f10a63412dc64783d3aa3049e5b49612e
nyamadori <nyamadorig@gmail.com>
2017-09-17 23:39:31 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910124108) do
+ActiveRecord::Schema.define(version: 20170917142002) do
 
   create_table "bots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "files"
   end
 
   create_table "execution_requests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -29,16 +30,6 @@ ActiveRecord::Schema.define(version: 20170910124108) do
     t.index ["hook_id"], name: "index_execution_requests_on_hook_id"
   end
 
-  create_table "hook_scripts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.bigint "hook_id", null: false
-    t.bigint "script_id", null: false
-    t.integer "order", default: 100, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["hook_id"], name: "index_hook_scripts_on_hook_id"
-    t.index ["script_id"], name: "index_hook_scripts_on_script_id"
-  end
-
   create_table "hooks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "description", null: false
     t.integer "kind", null: false
@@ -49,13 +40,5 @@ ActiveRecord::Schema.define(version: 20170910124108) do
     t.index ["bot_id"], name: "index_hooks_on_bot_id"
   end
 
-  create_table "scripts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "hook_scripts", "hooks"
-  add_foreign_key "hook_scripts", "scripts"
   add_foreign_key "hooks", "bots"
 end
