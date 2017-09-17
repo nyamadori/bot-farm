6c4438a282743d4b1e55ca9558d4bed27fbd5100
nyamadori <nyamadorig@gmail.com>
2017-09-18 00:36:43 +0900

    Add stub method

--- app/models/hook_kind/base.rb
+++ app/models/hook_kind/base.rb
@@ -1,5 +1,9 @@
 module HookKind
   class Base
     include ActiveModel::Model
+
+    def match(event)
+      raise NotImplementedError, 'this method needs to be overridden in subclass'
+    end
   end
 end
