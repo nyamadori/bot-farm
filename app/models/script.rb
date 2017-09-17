e91a140c1037f448fe2e9333de8e3a189218a03f
nyamadori <nyamadorig@gmail.com>
2017-09-17 23:12:02 +0900

    Remove script model

--- app/models/script.rb
+++
@@ -1,4 +0,0 @@
-class Script < ApplicationRecord
-  has_many :hook_scripts
-  has_many :hooks, through: :hook_scripts
-end
--- spec/factories/scripts.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :script do
-    content <<-SCRIPT
-// message.reply(arguments[0])
-    SCRIPT
-  end
-end
