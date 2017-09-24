1092205286934fd1514d59d00feb57d4bc7c3eda
nyamadori <nyamadorig@gmail.com>
2017-09-24 10:23:10 +0900

    Add create action

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -8,4 +8,20 @@ class BotsController < ApplicationController
   def new
     @bot = Bot.new
   end
+
+  def create
+    @bot = Bot.new(bot_params)
+
+    if @bot.save
+      redirect_to @bot
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bot_params
+    params.require(:bot).permit(:name, :description, :script, :attachments)
+  end
 end
